8a3b55cb539f5ba66b160c7777679a7772e5c8d1
Nithin Pradeep <nithinpradeep@Nithins-MacBook-Pro.local>
2024-01-07 00:49:54 -0800

    Column names added to tsv files

--- src/stage_02_feature_extraction.py
+++ src/stage_02_feature_extraction.py
@@ -4,6 +4,7 @@ import shutil
 from tqdm import tqdm
 import logging
 from src.utils.common import read_yaml, create_directories, get_df
+from src.utils.data_mgmt import save_matrix
 import random
 import numpy as np 
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
@@ -50,12 +51,14 @@ def main(config_path, params_path):
     tfidf= TfidfTransformer(smooth_idf=False)
     tfidf.fit(train_words_binary_matrix) #fitting TF-IDF transformer on train data
     train_words_tfidf_matrix= tfidf.transform(train_words_binary_matrix)
+    save_matrix(df=df_train,matrix= train_words_tfidf_matrix, out_path= featurized_train_data_path)
 
     #test
     df_test= get_df(test_data_path)
     test_words= np.array(df_test.text.str.lower().values.astype("U"))
     test_words_binary_matrix= bag_of_words.transform(test_words)
     test_words_tfidf_matrix= tfidf.transform(test_words_binary_matrix)
+    save_matrix(df_test,matrix= test_words_tfidf_matrix, out_path= featurized_test_data_path)
 
 
 
